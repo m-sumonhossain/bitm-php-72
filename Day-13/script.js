ef205e36c479bb5d4116df7bcf6e48732da783dc
sumon1019@gmail.com <sumon1019@gmail.com>
2017-12-22 22:49:43 +0600

    javascript-practice

---
+++ Day-13/script.js
@@ -0,0 +1,69 @@
+$('#img1').click(function () {
+    var imgUrl = $('#img1').attr('src');
+    $('#mainImage').attr('src',imgUrl);
+
+
+});
+$('#img2').click(function () {
+    var imgUrl = $('#img2').attr('src');
+    $('#mainImage').attr('src',imgUrl);
+
+
+});
+$('#img3').click(function () {
+    var imgUrl = $('#img3').attr('src');
+    $('#mainImage').attr('src',imgUrl);
+
+
+});
+$('#img4').click(function () {
+    var imgUrl = $('#img4').attr('src');
+    $('#mainImage').attr('src',imgUrl);
+
+
+});
+
+
+
+
+// $('#submitbtn').click(function () {
+//     var firstName = $('#firstName').val();
+//     var lastName = $('#lastName').val();
+//     var fullName = firstName+' '+lastName;
+//     $('#fullName').val(fullName);
+//
+// });
+
+
+
+// $('#h1').click(function () {
+//     $(this).css({
+//         'color':'red',
+//         'font-size': '5px',
+//         'background-color': 'red'
+//     });
+//
+// });
+
+
+
+
+
+
+
+
+// DOM Practice
+// var btnElement = document.getElementById('btn');
+// btnElement.onclick = function () {
+//
+//     var newHeadingElement = document.createElement('h1');
+//     var newParagraphElement = document.createElement('p');
+//
+//     newHeadingElement.innerHTML = 'This is a Heading';
+//     newParagraphElement.innerHTML = 'This is a Paragraph';
+//
+//     document.getElementById('div').appendChild(newHeadingElement);
+//     document.getElementById('div').appendChild(newParagraphElement);
+// }
+
+// DOM Practice End Here
