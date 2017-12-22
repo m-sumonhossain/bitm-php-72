96e9928f7dbcd4ab96ca3e0a163a6c71df0d5209
sumon1019@gmail.com <sumon1019@gmail.com>
2017-12-22 22:45:01 +0600

    javascript-practice

---
+++ day-12/js.js
@@ -0,0 +1,77 @@
+// var firstNameElement = document.getElementById('firstName');
+// firstNameElement.onkeyup =function (){
+//    var firstNameValue= document.getElementById('firstName').value;
+//    document.getElementById('result1').innerHTML = firstNameValue;
+//
+// }
+//
+// var lastNameElement = document.getElementById('lastName');
+// lastNameElement.onkeyup =function (){
+//     var lastNameValue= document.getElementById('lastName').value;
+//     document.getElementById('result2').innerHTML = lastNameValue;
+//
+// }
+//
+// lastNameElement.onblur=function (){
+//     var firstNameValue= document.getElementById('firstName').value;
+//     var lastNameValue= document.getElementById('lastName').value;
+//     document.getElementById('result3').innerHTML = firstNameValue+' '+lastNameValue;
+//
+// }
+// var btn = document.getElementById('btn');
+// btn.onclick=function () {
+//     var firstNum = document.getElementById('startingNumber').value;
+//     var lastNum = document.getElementById('endingNumber').value;
+//     var result = document.getElementById('result');
+//     var res='';
+//     for (var x=firstNum; x<=lastNum; x++){
+//         document.write(x+'');
+//         res+=x;
+//         result.innerHTML=res;
+//
+//     }
+//
+// }
+
+// var redBtn = document.getElementById('redBtn');
+// redBtn.onmouseover = function () {
+//     var divElement= document.getElementById('div');
+//     divElement.className = 'class-one';
+//
+// };
+
+// var greenBtn = document.getElementById('greenBtn');
+// greenBtn.onmouseover = function () {
+//     var divElement= document.getElementById('div');
+//     divElement.className = 'class-two';
+//
+// };
+//
+//
+// var blueBtn = document.getElementById('blueBtn');
+// blueBtn.onmouseover = function () {
+//     var divElement= document.getElementById('div');
+//     divElement.className = 'class-three';
+//
+// };
+
+
+var upBtn = document.getElementById('up');
+upBtn.onmouseover = function () {
+    var divElement= document.getElementById('div');
+    divElement.className = 'class-one';
+
+};
+var downBtn = document.getElementById('down');
+downBtn.onmouseover = function () {
+    var divElement= document.getElementById('div');
+    divElement.className = 'class-two';
+
+};
+
+
+
+
+
+
+
